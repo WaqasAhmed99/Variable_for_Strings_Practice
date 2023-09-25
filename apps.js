ce924986eaa51b94b97ed5117cb83f37b3b81a92
Waqas Ahmed <137869352+WaqasAhmed99@users.noreply.github.com>
2023-09-25 01:31:33 -0700

    Add files via upload
    SMIT  JavaScript Chapter: 2 (Variable-for-String) Assignment

---
+++ apps.js
@@ -0,0 +1,15 @@
+var myName = ("Waqas Ahmed")
+var myAge = (24)
+var message = ("Certified Web Application Developer")
+var email = ("waqas01949@gmail.com")
+var book = ("A smarter way to learn JavaScript")
+
+
+alert(myName)
+alert("I am "  + myAge + " Years old")
+alert(message)
+alert(email)
+alert(myName + "\n" + "I am" + myAge + " Years old" + "\n" + message + "\n" + email)
+alert("I am trying to learn from the Book " + book)
+document.write("Yah! I can write HTML content through JavaScript")
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
